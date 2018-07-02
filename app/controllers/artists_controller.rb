1d53aefb8676f7be90d104f89f386d724fb73c2c
Dee Race <deerace12@gmail.com>
2018-07-02 16:50:09 +0000

    automatically backed up by learn

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,8 +1,13 @@
 class ArtistsController < ApplicationController
+  before_action :set_preferences, only: [:index, :new]
 
-  def index
-    @artists = Artist.all
-  end
+   def index
+     if @preferences && @preferences.artist_sort_order
+       @artists = Artist.order(name: @preferences.artist_sort_order)
+     else
+       @artists = Artist.all
+     end
+   end
 
   def show
     @artist = Artist.find(params[:id])
